79407186384290cd5b8b5f053f2c29a00a968c87
crispycrip30 <al386763@uji.es>
2021-12-13 19:15:42 +0100

    Comienzo fase de compra en EM, no en BT

--- IAIA2/Assets/Scripts/EconomyManager.cs
+++ IAIA2/Assets/Scripts/EconomyManager.cs
@@ -5,36 +5,35 @@ using UnityEngine;
 public class EconomyManager : MonoBehaviour
 {
     //public GM gm;
+    public Grid grid;
 
     private void Update()
     {
-
+        //PurchasePhase();
+        SeeForUnitInCastle();
     }
 
     public void GiveMoneyForUnit(Unit unit) //dada la unidad a la que ha matado
     {
         int money = 0;
 
-        //dinero bajo
-        if (unit.unitType == "Guerrero Aliado" || unit.unitType == "Guerrero Enemigo")
+        if (unit.name.Contains("Guerrero"))
         {
             //la mitad del coste de creación
             money += 20;
         }
-        //dinero alto
-        else if (unit.unitType == "Volador Aliado" || unit.unitType == "Volador Enemigo")
+        else if (unit.name.Contains("Volador"))
         {
             //la mitad del coste de creación
             money += 40;
         }
-        //dinero medio
-        else //la unidad es Tanque Enemigo o Tanque Aliado
+        else //unit.name.Contains("Tanque")
         {
             //la mitad del coste de creación
-            money += 35;
+            money += 25;
         }
 
-        if (GameObject.Find("Map Generator").GetComponent<GM>().playerTurn == 1) //turno enemigo
+        if (GameObject.Find("Map Generator").GetComponent<GM>().playerTurn == 1) //turno enemigo (1)
         {
             GM.player1Gold += money;
         }
@@ -74,4 +73,87 @@ public class EconomyManager : MonoBehaviour
 
         return necessaryMoney;
     }
+
+    private void PurchasePhase()
+    {
+
+        int tank = 0;
+        int flying = 0;
+        int warrior = 0;
+
+        int totalFeedingCost = 0;
+
+        //identificamos cuantas unidades de cada tipo tiene la IA
+        for (int i = 0; i < MapGenerator.unitsEnemy.Count; i++)
+        {
+            if (MapGenerator.unitsEnemy[i].name.Contains("Tanque"))
+            {
+                tank++;
+            }
+            else if (MapGenerator.unitsEnemy[i].name.Contains("Guerrero"))
+            {
+                warrior++;
+            }
+            else
+            {
+                flying++;
+            }
+
+            totalFeedingCost += MapGenerator.unitsEnemy[i].gameObject.transform.GetComponent<Unit>().feedingCost;
+
+        }
+
+        if (flying == 0)
+        {
+            //si podemos asumir el gasto y además tenemos dinero suficiente como para mantener a las unidades por otros 4 turnos
+            if (GM.player1Gold - 80 >= totalFeedingCost*4 + 5) //el coste total de manutención más el coste de manutención de la nueva unidad a comprar
+            {
+
+            }
+        }
+        else if(tank == 0)
+        {
+
+        }
+        //else if(warrior == 0) //peor unidad, no me interesa comprarla a no ser que esté en una situación crítica
+        //{
+
+        //}
+    }
+
+    //private string WhatUnit(int count)
+    //{
+
+    //}
+
+    private void SeeForUnitInCastle()
+    {
+        List<Nodo> vecinos;
+
+        vecinos = GameObject.Find("Map Generator").GetComponent<Grid>().GetAllNeighbouringNodes(MapGenerator.nodoCastilloEnemigo);
+
+        //para cada unidad enemiga vamos a comprobar si está cerca del castillo
+        foreach (Nodo nodo in vecinos)
+        {
+            for (int i = 0; i < MapGenerator.unitsEnemy.Count; i++)
+            {
+                Debug.Log(grid.NodeFromWorldPosition(MapGenerator.unitsEnemy[i].transform.position) == nodo);
+                if (grid.NodeFromWorldPosition(MapGenerator.unitsEnemy[i].transform.position) == nodo)
+                {
+
+                }
+            }
+        }
+    }
+
+    //private bool UnitInADeterminateTile(Nodo nodo, Unit unit)
+    //{
+    //    if (nodo.tile.GetComponent<Tile>().
+
+    //    {
+
+    //    }
+
+    //    return true;
+    //}
 }
